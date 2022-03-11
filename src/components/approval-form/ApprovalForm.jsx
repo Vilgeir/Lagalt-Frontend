5e4e03cc1040946656c95160018919554e155250
Mohamud-abdi <yare.70@hotmail.com>
2022-03-11 09:19:45 +0100

    merging to main

--- src/components/approval-form/ApprovalForm.jsx
+++ src/components/approval-form/ApprovalForm.jsx
@@ -4,8 +4,8 @@ import './ApprovalForm.css'
 
 export const ApprovalForm = () => {
   return (
-    <div>
-      <form >
+    <div className="classWrapper">
+      <form>
       
         <textarea  type="text" placeholder="Hva motiverer deg for å søke dette prosjektet?" className="form"></textarea>
         <button className="button"> send søknaden</button>
