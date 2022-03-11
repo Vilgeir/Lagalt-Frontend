b5f5bfa8257900bcc76afdd8b001fb86d6dd9a5c
Mohamud-abdi <yare.70@hotmail.com>
2022-03-11 11:00:27 +0100

    adding  home and profile navigation

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 import "./navbar.css";
 
 const NavBar = () => {
+  const [profile, setProfile] = useState(false);
+
+  const toggleProfileHandler = () => {
+    setProfile((prevProfile) => !prevProfile);
+  };
+
+
+
   return (
     <div>
       <nav className="navbar-container">
-        <h3>Username</h3>
-        <button id="login-button">Login</button>
+        <h3><Link to={"/"}>Home</Link></h3>
+        <button id="login-button" onClick={toggleProfileHandler}>
+          {profile ? <Link to={"/profile"}>Profile</Link> : "Login"}
+        </button>
       </nav>
-	 
-	  <div>
-	  
-	  </div>
+
+      <div></div>
     </div>
   );
 };
