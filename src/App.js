5b2cc485c1d1ea9593f0bb09948ea892aa4f8b9b
Vilgeir <vilgeir@gmail.com>
2022-03-14 16:26:37 +0100

    created create page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from "./pages/Main";
-import Profile from "./pages/Profile";
-import NavBar from "./components/navbar/NavBar";
+import "./App.css"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Main from "./pages/Main"
+import Profile from "./pages/Profile"
+import NavBar from "./components/navbar/NavBar"
+import Create from "./pages/Create"
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <div className="app-container">
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
-};
+	return (
+		<BrowserRouter>
+			<NavBar />
+			<div className="app-container">
+				<Routes>
+					<Route path="/" element={<Main />} />
+					<Route path="/profile" element={<Profile />} />
+					<Route path="/create" element={<Create />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
+	)
+}
 
-export default App;
+export default App
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,15 +1,14 @@
-import React from "react";
-import { FilterProject } from "../components/Filter/FilterProjects";
-import ProjectList from "../components/project-list/ProjectList";
+import React from "react"
+import FilterProject from "../components/filter/FilterProjects"
+import ProjectList from "../components/project-list/ProjectList"
 
 const Main = () => {
-  return (
-    <>
-      <FilterProject />
+	return (
+		<>
+			<FilterProject />
+			<ProjectList />
+		</>
+	)
+}
 
-      <ProjectList />
-    </>
-  );
-};
-
-export default Main;
+export default Main
