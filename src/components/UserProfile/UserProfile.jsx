b42f1fecd94355ab7faff892d3f15bf922e4d758
Ludvig Hansen <lubuha@hotmail.com>
2022-03-14 16:02:57 +0100

    user profile and settings skeleton

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,47 +1,35 @@
-
-import './UserProfile.css'
+import "./UserProfile.css"
 
 export const UserProfile = () => {
-  return (
-    <div className='wrapper'>
-      <div class="card-container">
-        <span class="pro">PRO</span>
-        <img
-          class="round"
-          src="https://randomuser.me/api/portraits/men/78.jpg"
-          alt="user"
-        />
-        <h3>User Name</h3>
-        <h6>Oslo</h6>
-        <p>
-          User interface designer and <br /> front-end developer
-        </p>
-      
-        <div class="portofolio">
-          <h3>Portfolio</h3>
-          <ul>
-            <li>Project Name</li>
-            <li>Description</li>
-            <li>Category</li>
-          </ul>
-        </div>
-        <div class="skills">
-          <h6>Skills</h6>
-          <ul>
-            <li>UI / UX</li>
-            <li>Front End Development</li>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>React</li>
-            <li>Node</li>
-          </ul>
-        </div>
-      </div>
+	return (
+		<section id="profile-container" className="main-content">
+			<div id="profile-header-container">
+				<img id="avatar" src="http://identicon.net/img/identicon.png"></img>
+				<h1>Navn Navnesen</h1>
+				<div id="profile-settings-button">
+					<span>X</span>
+				</div>
+			</div>
+			<h2>Beskrivelse:</h2>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, sit
+				quam? Fugit necessitatibus eos provident adipisci, blanditiis mollitia
+				cum quia quis possimus officiis ea maiores laborum debitis nobis
+				laudantium accusamus, dolor nulla culpa suscipit dolores enim libero
+				quasi! Non, ipsa.
+			</p>
 
-      <footer>
-      
-      </footer>
-    </div>
-  );
-};
+			<div id="profile-projects-container">
+				<h2>Prosjekter</h2>
+				<div>
+					<span>Pågående:</span>
+					<div id="profile-current-projects-container"></div>
+				</div>
+				<div>
+					<span>Ferdige:</span>
+					<div id="profile-closed-projects-container"></div>
+				</div>
+			</div>
+		</section>
+	)
+}
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { UserProfile } from "../components/UserProfile/UserProfile";
+import React from "react"
+import { UserProfile } from "../components/UserProfile/UserProfile"
 
 const ProfilePage = () => {
-  return <UserProfile />;
-};
+  return <UserProfile />
+}
 
-export default ProfilePage;
+export default ProfilePage
