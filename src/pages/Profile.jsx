aaa542674c2eed66a22e9dff0131e3995baeb664
Ludvig Hansen <lubuha@hotmail.com>
2022-03-16 11:34:50 +0100

    user settings component styling

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,14 @@
 import React from "react"
+import { UserSettings } from "../components/user-settings/UserSettings"
 import { UserProfile } from "../components/UserProfile/UserProfile"
 
 const ProfilePage = () => {
-	return <UserProfile />
+	return (
+		<>
+			<UserProfile />
+			<UserSettings />
+		</>
+	)
 }
 
 export default ProfilePage
