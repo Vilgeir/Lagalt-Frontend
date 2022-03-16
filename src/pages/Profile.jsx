78b1ca18de250531ccb7c5afabf585f82517e93e
Ludvig Hansen <lubuha@hotmail.com>
2022-03-16 15:29:56 +0100

    removed placeholder

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,10 @@
 import React from "react"
-import { UserSettings } from "../components/user-settings/UserSettings"
 import { UserProfile } from "../components/UserProfile/UserProfile"
 
 const ProfilePage = () => {
 	return (
 		<>
 			<UserProfile />
-			<UserSettings />
 		</>
 	)
 }
