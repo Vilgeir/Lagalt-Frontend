e609c7f102fb441c99eceeaf98ffb4aa17441fce
Mohamud-abdi <yare.70@hotmail.com>
2022-03-17 23:38:40 +0100

    updating apicontext

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import FilterProjects from "../components/filter/FilterProjects"
+import FilterProjects from "../components/Filter/FilterProjects"
 import ProjectList from "../components/project-list/ProjectList"
 
 const Main = () => {
