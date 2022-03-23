6a91f6f106c849aa833cdcaf7fd9bf84fa068100
Ludvig Hansen <lubuha@hotmail.com>
2022-03-23 14:04:59 +0100

    Create post method and styling

--- src/api/create-project.js
+++ src/api/create-project.js
@@ -1,19 +1,37 @@
-export const createProject = async (formData) => {
+export const createProject = async (formData, skills) => {
 	try {
 		const response = await fetch(
-			"https://lagalt.azurewebsites.net/api/Projects",
+			"https://lagalt.azurewebsites.net/api/Projects/",
 			{
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
 				body: JSON.stringify({
 					projectTitle: formData.projectTitle,
 					description: formData.description,
+					photo: formData.photo,
 					progress: "founding",
+					users: [1],
+					skills: skills,
+					projectLeaderIds: [1],
+					fieldId: formData.fieldId,
 				}),
 			}
 		)
+		console.log(
+			JSON.stringify({
+				projectTitle: formData.projectTitle,
+				description: formData.description,
+				photo: formData.photo,
+				progress: "founding",
+				users: [1],
+				skills: skills,
+				projectLeaderIds: [1],
+				fieldId: formData.fieldId,
+			})
+		)
 		if (!response.ok) {
-			throw new Error("could not create project.")
+			console.log(response)
+			throw new Error(response.error)
 		}
 
 		const data = await response.json
